4a51dbc33f21b2e8a4cd3dab2a34e4845e194961
= <=>
2026-04-03 16:45:57 +0530

    Chages the new name-Done in service file

--- src/webparts/newShortcutsApp/components/Services/ApiService.ts
+++ src/webparts/newShortcutsApp/components/Services/ApiService.ts
@@ -53,7 +53,7 @@ export function fetchBestTabData(userEmail: string): Promise<ITabResult> {
     .then((response: Response) => {
       if (!response.ok) {
         throw new Error(
-          `BEST API error: ${response.status} ${response.statusText}`,
+          `BES API error: ${response.status} ${response.statusText}`,
         );
       }
       return response.json();
